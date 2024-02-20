cabb0c0c492501f2a430ee5b4c61de4739eb6928
Suraj Divekar <100984211+surajdivekar@users.noreply.github.com>
2024-02-20 14:02:51 +0530

    multer unique name file name change

--- backend/src/controllers/multer.js
+++ backend/src/controllers/multer.js
@@ -5,12 +5,20 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/uploads");
   },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
+  // filename: function (req, file, cb) {
+  //   cb(
+  //     null,
+  //     file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+  //   );
+  // },
+  filename: (req, file, cb) => {
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/:/g, "-")
+      .replace(/\s+/g, "");
+    cb(null, timestamp + "-" + file.originalname);
   },
+
 });
 
 const upload = multer({
